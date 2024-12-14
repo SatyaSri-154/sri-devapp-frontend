8ca3e810aa07c9b0dac67a82c5d9fd056450f22a
lakshmi satya sri Saladi <139320349+SatyaSri-154@users.noreply.github.com>
2024-12-14 21:08:55 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("mysql","root","sri1234","loginapp");
+    $con = mysqli_connect("mysql","root","sri1234","register");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
